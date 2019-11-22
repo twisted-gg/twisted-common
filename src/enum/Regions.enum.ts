839c444daff9a2ab1847cb10bd0897222a042833
Sansossio <juliosansossio@gmail.com>
2019-11-22 20:10:19 +0100

    Available regions const

---
+++ src/enum/Regions.enum.ts
@@ -0,0 +1,5 @@
+import { Constants } from 'twisted'
+
+export const RegionsList = [
+  Constants.Regions.LAT_NORTH
+]
--- src/enum/index.ts
+++ src/enum/index.ts
@@ -1,3 +1,4 @@
 export * from './OriginMiddleware.enum'
 export * from './Environment.enum'
 export * from './Cache.enum'
+export * from './Regions.enum'
