8a9c1081c00bd1771745f7b61cd485db23d0ad34
Sansossio <juliosansossio@gmail.com>
2019-11-26 23:06:02 +0100

    export wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import * as wrapper from './wrapper'
+
 export * from './config'
 export * from './enum'
 export * from './middleware'
 export * from './services'
+
+export const Wrapper = wrapper
--- src/wrapper/index.ts
+++ src/wrapper/index.ts
@@ -1,2 +1,1 @@
 export * from 'twisted'
-export * from 'twisted/dist'
